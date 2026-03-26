f9abe3de0844c579e01c8346e97797fc80395f81
costta <lukasgaby2016@gmail.com>
2026-03-26 16:54:40 -0300

    inicio do react

---
+++ projeto/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import './App.css'
+import Mensagem from './components/Mensagem'
+
+function App() {
+  
+  return (
+    <>
+      <h1>Bem-vindo ao meu projeto</h1>
+
+
+      <Mensagem/>
+      <Mensagem/>
+      <Mensagem/>
+      <Mensagem/>
+    </>
+  )
+}
+
+export default App
---
+++ projeto/src/components/Mensagem/index.jsx
@@ -0,0 +1,14 @@
+import './style.css'
+
+function Mensagem(params) {
+    return(
+        //comandos HTML.
+        <div className='mensagem'>
+            <h1>Componente </h1>
+            <p>Este é um componete com uma mensagem simples.</p>
+        </div>
+    )
+    
+}
+
+export default Mensagem
